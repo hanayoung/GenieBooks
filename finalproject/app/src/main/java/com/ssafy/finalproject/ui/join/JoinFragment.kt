09de8a67a5476da7f90ea075657e0530088181d5
민수현 <minsh2344@gmail.com>
2024-11-20 17:26:58 +0900

    Feat: 회원가입 화면 구현

--- finalproject/app/src/main/java/com/ssafy/finalproject/ui/join/JoinFragment.kt
+++ finalproject/app/src/main/java/com/ssafy/finalproject/ui/join/JoinFragment.kt
@@ -14,5 +14,16 @@ class JoinFragment : BaseFragment<FragmentJoinBinding>(
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        // Male 클릭 시 상태 변경
+        binding.ivMale.setOnClickListener {
+            binding.ivMale.setImageResource(R.drawable.ic_male_checked)  // 남성 선택 이미지
+            binding.ivFemale.setImageResource(R.drawable.ic_female)  // 여성 비선택 이미지
+        }
+
+        // Female 클릭 시 상태 변경
+        binding.ivFemale.setOnClickListener {
+            binding.ivFemale.setImageResource(R.drawable.ic_female_checked)  // 여성 선택 이미지
+            binding.ivMale.setImageResource(R.drawable.ic_male)  // 남성 비선택 이미지
+        }
     }
 }
