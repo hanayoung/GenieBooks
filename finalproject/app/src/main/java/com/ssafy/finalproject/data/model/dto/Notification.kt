2e30a10c9892b25e5027b16452d5835d7b5751a6
hay6586 <hay6586@gmail.com>
2024-11-21 16:53:56 +0900

    notification fragment ui

---
+++ finalproject/app/src/main/java/com/ssafy/finalproject/data/model/dto/Notification.kt
@@ -0,0 +1,7 @@
+package com.ssafy.finalproject.data.model.dto
+
+data class Notification(
+    val title : String,
+    val content : String,
+    val date : Long
+)
---
+++ finalproject/app/src/main/java/com/ssafy/finalproject/ui/mypage/fragment/NotificationListFragment.kt
@@ -0,0 +1,18 @@
+package com.ssafy.finalproject.ui.mypage.fragment
+
+import android.os.Bundle
+import android.view.View
+import com.ssafy.finalproject.R
+import com.ssafy.finalproject.base.BaseFragment
+import com.ssafy.finalproject.databinding.FragmentNotificationListBinding
+
+class NotificationListFragment : BaseFragment<FragmentNotificationListBinding>(
+    FragmentNotificationListBinding::bind,
+    R.layout.fragment_notification_list
+) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
