5c8e1a93ea8663e3791323e1a1e417921906f0db
minsh2344 <minsh2344@gmail.com>
2024-11-21 21:33:11 +0900

    Feat: 홈 화면 마무리 및 픽업 대기 목록 화면 구현

---
+++ android_staff/app/src/main/java/com/ssafy/myapplication/ui/orderlist/OrderListFragment.kt
@@ -0,0 +1,12 @@
+package com.ssafy.myapplication.ui.orderlist
+
+import com.ssafy.myapplication.R
+import com.ssafy.myapplication.base.BaseFragment
+import com.ssafy.myapplication.databinding.FragmentHomeBinding
+import com.ssafy.myapplication.databinding.FragmentOrderListBinding
+
+class OrderListFragment: BaseFragment<FragmentOrderListBinding>(
+    FragmentOrderListBinding::bind,
+    R.layout.fragment_order_list
+) {
+}
