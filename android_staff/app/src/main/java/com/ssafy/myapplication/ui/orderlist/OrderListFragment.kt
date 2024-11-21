69fd67db2f780862fb651962549ba32f4f6ae085
minsh2344 <minsh2344@gmail.com>
2024-11-21 22:59:55 +0900

    주문 상세 화면 구현

--- android_staff/app/src/main/java/com/ssafy/myapplication/ui/orderlist/OrderListFragment.kt
+++ android_staff/app/src/main/java/com/ssafy/myapplication/ui/orderlist/OrderListFragment.kt
@@ -2,10 +2,9 @@ package com.ssafy.myapplication.ui.orderlist
 
 import com.ssafy.myapplication.R
 import com.ssafy.myapplication.base.BaseFragment
-import com.ssafy.myapplication.databinding.FragmentHomeBinding
 import com.ssafy.myapplication.databinding.FragmentOrderListBinding
 
-class OrderListFragment: BaseFragment<FragmentOrderListBinding>(
+class OrderListFragment : BaseFragment<FragmentOrderListBinding>(
     FragmentOrderListBinding::bind,
     R.layout.fragment_order_list
 ) {
