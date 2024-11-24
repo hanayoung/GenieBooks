8bbc3c685538128fd8a4bc0949fedcd713e7d126
aoreo <hay6586@gmail.com>
2024-11-24 15:50:27 +0900

    선물카드 제작 ui

--- finalproject/app/src/main/java/com/ssafy/finalproject/ui/gift/GiftCardFragment.kt
+++ finalproject/app/src/main/java/com/ssafy/finalproject/ui/gift/GiftCardFragment.kt
@@ -1,27 +1,92 @@
 package com.ssafy.finalproject.ui.gift
 
+import android.Manifest
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings.Global
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
 import com.ssafy.finalproject.R
 import com.ssafy.finalproject.base.BaseFragment
 import com.ssafy.finalproject.databinding.FragmentGiftCardBinding
+import com.ssafy.finalproject.util.PermissionChecker
 
+private const val TAG = "GiftCardFragment"
 class GiftCardFragment : BaseFragment<FragmentGiftCardBinding>(
     FragmentGiftCardBinding::bind,
     R.layout.fragment_gift_card
 ) {
+
+    /** permission check **/
+    private val checker = PermissionChecker(this)
+    private val runtimePermissionsAfterT = arrayOf(Manifest.permission.READ_MEDIA_IMAGES)
+    private val runtimePermissionsBeforeT = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    /** permission check **/
+    private lateinit var launcher: ActivityResultLauncher<Intent>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        checkPermission()
+
+        launcher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == AppCompatActivity.RESULT_OK) {
+                val intent = checkNotNull(result.data)
+                val imageUri = intent.data // 갤러리에서 선택한 사진 받아옴
+
+                Glide.with(requireContext())
+                    .load(imageUri)
+                    .centerCrop()
+                    .into(binding.image)
+            }
+        }
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         binding.btnAddImg.setOnClickListener {
             // 갤러리 연결
+            val intent = Intent().also { intent ->
+                intent.type = "image/"
+                intent.action = Intent.ACTION_GET_CONTENT
+            }
+            launcher.launch(intent)
         }
 
         binding.btnSendGift.setOnClickListener {
             // 서버로 선물카드 전송 + 구매 목록 전송
+        }
+    }
+
+    private fun checkPermission(){
+        /** permission check **/
+            if (!checker.checkPermission(requireContext(), arrayOf(getRequiredPermission()))) {
+                checker.setOnGrantedListener { //퍼미션 획득 성공일때
+                    Log.d(TAG, "checkPermission: permission granted")
+                }
+                checker.requestPermissionLauncher.launch(arrayOf(getRequiredPermission())) // 권한없으면 창 띄움
+            } else { //이미 전체 권한이 있는 경우
+                Log.d(TAG, "checkPermission: permission 이미 있음")
+            }
+
+        /** permission check **/
+    }
+
+    private fun getRequiredPermission(): String {
+        return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            Manifest.permission.READ_MEDIA_IMAGES
+        } else {
+            Manifest.permission.READ_EXTERNAL_STORAGE
         }
     }
 }
