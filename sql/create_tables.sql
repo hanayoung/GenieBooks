bd278e423dc2abf3be3389a0dbe63a0c538b5aab
minsh2344 <minsh2344@gmail.com>
2024-11-25 23:35:06 +0900

    sql

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -5,10 +5,10 @@ use ssafy_pennant;
 create table customer(
     c_id integer auto_increment primary key,
     nickname char(50) not null,
-	id char(50) not null,
+    id char(50) not null,
     pwd char(50) not null,
     age integer not null default 1,
-	sex char(50) not null,
+    sex char(50) not null,
     point integer not null default 0
 );                                                 
 
@@ -22,7 +22,7 @@ create table interest(
 create table staff(
     s_id integer auto_increment primary key,
     nickname char(50) not null,
-	id char(50) not null,
+    id char(50) not null,
     pwd char(50) not null
 );                                                 
                    
@@ -30,14 +30,11 @@ create table p_customer(
     o_id integer auto_increment primary key,
     c_id integer not null,
     o_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    p_date TIMESTAMP,
-    is_pickup boolean not null default false,
-    is_done boolean not null default false,
-    payment integer not null default 0
+    is_done boolean not null default false
 );                               
 
 create table p_book(
-	id integer auto_increment primary key,
+    id integer auto_increment primary key,
     isbn bigint,
     o_id integer not null,
     quantity integer not null default 1,
@@ -48,22 +45,21 @@ create table gift(
     g_id integer auto_increment primary key,
     title char(50) not null,
     content char(100) not null,
-    image char(255) not null default false,
-	g_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    image char(255) not null,
+    g_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     recipient_id int not null default 0,
     sender_id int not null,
-    constraint foreign key (sender_id) references customer(c_id) on delete cascade
     sender_name varchar(50),
     constraint foreign key (sender_id) references customer(c_id) on delete cascade
-);
+); 
 
 create table recommend(
-	id integer auto_increment primary key,
+    id integer auto_increment primary key,
     isbn bigint not null
 );
 
 create table attendance(
-	id integer auto_increment primary key,
+    id integer auto_increment primary key,
     c_id integer not null,
     attend_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     constraint foreign key (c_id) references customer(c_id) on delete cascade
@@ -71,6 +67,3 @@ create table attendance(
 
 ALTER TABLE customer ADD COLUMN fcm_token varchar(255) NOT NULL;
 
-ALTER TABLE p_customer ADD COLUMN is_pickup boolean NOT NULL default false;
-
-ALTER TABLE p_customer ADD COLUMN pickup_date TIMESTAMP;
