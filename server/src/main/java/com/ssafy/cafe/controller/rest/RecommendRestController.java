5ee0acfdaff4720ba81b6d4a60dcec1eea9247f8
hay6586 <hay6586@gmail.com>
2024-11-26 18:18:30 +0900

    gpt api 연결

--- server/src/main/java/com/ssafy/cafe/controller/rest/RecommendRestController.java
+++ server/src/main/java/com/ssafy/cafe/controller/rest/RecommendRestController.java
@@ -1,5 +1,6 @@
 package com.ssafy.cafe.controller.rest;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,12 +8,15 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 
 import com.ssafy.cafe.model.dto.BookRecommendRequest;
 import com.ssafy.cafe.model.dto.BookRecommendation;
@@ -20,10 +24,12 @@ import com.ssafy.cafe.model.dto.Category;
 import com.ssafy.cafe.model.dto.Customer;
 import com.ssafy.cafe.model.dto.GiftCard;
 import com.ssafy.cafe.model.dto.GoogleBook;
+import com.ssafy.cafe.model.dto.GoogleBookResponse;
 import com.ssafy.cafe.model.service.BookParser;
 import com.ssafy.cafe.model.service.CustomerService;
 import com.ssafy.cafe.model.service.GPTService;
 import com.ssafy.cafe.model.service.GiftCardService;
+import com.ssafy.cafe.util.Constants;
 
 import io.swagger.v3.oas.annotations.Operation;
 
@@ -56,11 +62,34 @@ public class RecommendRestController {
 			List<BookRecommendation> recommendBooks = BookParser.parseBookRecommendations(recommendBooksString);
 			logger.debug("result : {}", recommendBooksString);
 			logger.debug("books : {}",Arrays.toString(recommendBooks.toArray()));
+			return searchGoogleBooks(recommendBooks);
+			
 		} catch (Exception e) {
 			logger.debug("error : {}", e.getMessage());
+			return new ArrayList<>();
 		}
 
-		return new ArrayList<>();
+		
+	}
+	
+	public List<GoogleBook> searchGoogleBooks(List<BookRecommendation> recommendBooks) {
+		RestTemplate restTemplate = new RestTemplate();
+		List<GoogleBook> aiRecommendBookList = new ArrayList<>();
+		try {
+			for (BookRecommendation recommendBook : recommendBooks) {
+				URI uri = UriComponentsBuilder.fromUriString(Constants.GOOGLE_BOOK_API_URL)
+						.queryParam("q", String.format("intitle:\"%s\"", recommendBook.getTitle())).queryParam("maxResults", 1).encode().build().toUri();
+				ResponseEntity<GoogleBookResponse> response = restTemplate.getForEntity(uri, GoogleBookResponse.class);
+
+				if(response != null && response.getBody() != null && response.getBody().getItems() != null) {
+					aiRecommendBookList.addAll(response.getBody().getItems());
+				}
+			}
+		} catch (Exception e) {
+			logger.debug("exception occur : {}", e.getMessage());
+		}
+
+		return aiRecommendBookList;
 	}
 
 }
