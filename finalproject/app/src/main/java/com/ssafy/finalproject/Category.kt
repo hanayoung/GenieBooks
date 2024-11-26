ae71f6961e81501000056c15acccc8eed2f0ec0f
민수현 <minsh2344@gmail.com>
2024-11-26 10:21:01 +0900

    Feat: 카테고리 변경

--- finalproject/app/src/main/java/com/ssafy/finalproject/Category.kt
+++ finalproject/app/src/main/java/com/ssafy/finalproject/Category.kt
@@ -7,18 +7,17 @@ enum class Category(private val categoryKr: String) {
     HEALTH("건강"),
     HIANDSOC("역사/문화"),
     TRAVEL("여행"),
-    BUANDEC("경제/경영"),
     CRANDHOANDFI("취미/실용/스포츠"),
     TEANDEN("기술/공학"),
     RELIGION("종교"),
     POANDLI("시/에세이"),
     ARTANDPER("예술/대중문화"),
-    HOANDHO("가정/육아"),
     COMPUTER("컴퓨터/IT"),
     HUMAN("인문"),
     SELFHELP("자기계발"),
     POANDSO("정치/사회"),
-    FOREIGN("외국어");
+    FOREIGN("외국어"),
+    JUVENILE("청소년");
 
     fun getCategoryKr(): String {
         return categoryKr
